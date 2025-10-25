c45af01088b2c1606f0b162eee2750dd8692e0f5
charo <yuya1charo@gmail.com>
2025-10-25 17:55:06 +0900

    postgres prismaORM set up

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,16 @@
+import "dotenv/config";
 import fastify from "fastify";
+import { PrismaClient } from "./generated/prisma/client.js";
 
+/**
+ * server
+ */
 const server = fastify();
+
+/**
+ * prisma
+ */
+const prisma = new PrismaClient();
 
 server.get("/ping", async (request, reply) => {
   return "pong\n";
@@ -13,3 +23,19 @@ server.listen({ port: 8080 }, (err, address) => {
   }
   console.log(`Server listening at ${address}`);
 });
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
