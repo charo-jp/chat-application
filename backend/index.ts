46d1d658e731a9a2b8afd93877b0ae31fdd4ffc7
charo <yuya1charo@gmail.com>
2026-02-24 21:54:11 +0900

    Jule試しにコミット

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import fastify from "fastify";
-import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaClient, type User } from "./generated/prisma/client.ts";
+import { PrismaClientKnownRequestError } from "./generated/prisma/internal/prismaNamespace.ts";
 
 /**
  * server
@@ -12,8 +13,26 @@ const server = fastify();
  */
 const prisma = new PrismaClient();
 
-server.get("/ping", async (request, reply) => {
-  return "pong\n";
+/**
+ * Get A User Information
+ */
+server.get<{ Params: { id: string } }>("/user/:id", async (request, reply) => {
+  // It needs to be aligned with key's name on a table
+  const { id } = request.params;
+
+  try {
+    const user = await prisma.user.findFirstOrThrow({
+      where: { id: id },
+    });
+
+    if (!user) {
+      return reply.status(404).send({ error: "User not found" });
+    }
+
+    return user;
+  } catch (error) {
+    return reply.status(500).send({ error: "Internal Server Error" });
+  }
 });
 
 server.listen({ port: 8080 }, (err, address) => {
